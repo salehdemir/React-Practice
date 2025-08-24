d840f480bf1259b1dd658103033af4fffac50d5a
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-08-24 10:42:04 +0430

    ternary operator

---
+++ my-app1/src/Register.jsx
@@ -0,0 +1,12 @@
+function Register(){
+return(
+    <>
+    <h1>Register</h1>
+     < input type='text' placeholder='enter your name'/>
+      < input type='password' placeholder='enter your password'/>
+      < input type='password' placeholder='confirm your password'/>
+      <button type='submit'>Register</button>
+    </>
+)
+}
+export default Register
---
+++ my-app1/src/login.jsx
@@ -0,0 +1,12 @@
+function Login(){
+    return(
+        <>
+        <h1>Login</h1>
+       < input type='text' placeholder='enter your name'/>
+       < input type='password' placeholder='enter your password'/>
+      <button type='submit'>Login</button>
+      </>
+    )
+}
+
+export default Login
