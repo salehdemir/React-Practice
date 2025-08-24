97f9beeb2fd8401893ae11bbb58bc7f1ae0f1421
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-08-24 10:41:01 +0430

    regiter and login

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,26 +1,57 @@
 import './App.css';
+import Login from './login';
+import Register from './Register';
 
-const myStyle = {
 
-backgroundColor: "blue"
-}
+
+// 3. var register = false;
+
+// function isRegister(){
+ 
+//   if(register){
+// return <Register/>
+//   }else{
+//  return   <Login/>
+//   }
+
+// }
 
 function App() {
+
+  var register = false;
   return (
-    // we should have div b/c it just render one element
-    <div className="App">
-     <h1 style={{color: "red"}}>Hello </h1>
-     <p style={myStyle}>lorem dsjflkdsjfa</p>
-     
-    </div>
+ 
+   <div className='container'>
+    <form className='form'>
+{register ? <Register/> : <Login/>}
+    </form>
+   </div>
+    
   );
 }
 
 export default App;
 
-// inline style is not working like before in js6 it working like object .
+
+
+
+
+// 1: inline style is not working like before in js6 it working like object .
 // const myStyle = {
 // color: "red" 
 // }
 // Or
 // <h1 style={{color: "red"}}>Hello World</h1>.
+
+
+//2: const myStyle = {
+
+// backgroundColor: "blue"
+// }
+
+   // 2: we should have div b/c it just render one element
+    // <div className="App">
+    //  <h1 style={{color: "red"}}>Hello </h1>
+    //  <p style={myStyle}>lorem dsjflkdsjfa</p>
+     
+    // </div>
