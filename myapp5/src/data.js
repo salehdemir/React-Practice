17abf8f39d9f8f824e7407b23f06c12b5d8daeae
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-08-25 14:20:26 +0430

    practice destructuring of object and array

---
+++ myapp5/src/data.js
@@ -0,0 +1,11 @@
+const animals = [
+  { name: "cat",
+    sound: "meow",
+    feeding : {
+      food : 2,
+      water : 4
+    }
+  },
+  { name: "dog", sound: "woof" }
+];
+export default animals
--- myapp5/src/main.jsx
+++ myapp5/src/main.jsx
@@ -1,11 +1,53 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './App.css'
-import App from './App'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import cars from "./parctice.js"
 
 
-const root = createRoot(document.getElementById('root')) 
+// var [cat, dog] = animals
+// var {name, sound, feeding : {food : catfood, water : catwater}} = cat
+// console.log(catfood)
+
+
+var [honda , tesla] = cars
+
+var { speedStats : {topSpeed : hondaTopSpeed}} = honda
+var { speedStats : {topSpeed : teslaTopSpeed}} = tesla
+var { coloursByPopularity : [hondaTopColour]} = honda
+var { coloursByPopularity : [teslaTopColour]} = tesla
+
+
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
 
-)
+    <table>
+      <thead>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>{tesla.model}</td>
+      { <td>{teslaTopSpeed}</td> }
+      {<td>{teslaTopColour}</td> }
+    </tr>
+    </tbody>
+    <tfoot>
+    <tr>
+      <td>{honda.model}</td>
+      { <td>{hondaTopSpeed}</td> }
+      { <td>{hondaTopColour}</td> }
+    </tr>
+    </tfoot>
+  </table>
+);
+
