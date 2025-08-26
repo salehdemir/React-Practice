21c008f3dbba8f591380589e04607f1e9308aeaa
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-08-26 15:35:17 +0430

    hover state

--- myapp5/src/App.jsx
+++ myapp5/src/App.jsx
@@ -3,16 +3,25 @@ import { useState } from 'react';
 
 function App() {
    const [first, second] = useState("Hello")
+   const [firstStyle, hoverStyle] = useState(false)
   
   function changeText (){
     second("bye")
   }
+  function hover (){
+    hoverStyle(true)
+  }
+  function out (){
+    hoverStyle(false)
+  }
+
+
 
   return (
-<div>
+<div> 
     <h1>{first}</h1>
   <input type="text" placeholder='Enter your name' />
-  <button onClick={changeText}>Submit</button>
+  <button onClick={changeText} onMouseOut={out} onMouseOver={hover} style={{backgroundColor : firstStyle ? "black" : "white" }}>Submit</button>
 </div>
   );
 }
