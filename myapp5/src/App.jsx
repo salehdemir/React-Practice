56a33c49da92c643306454dcc428360e7cacfcab
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-08-27 14:07:59 +0430

    usestate change text

--- myapp5/src/App.jsx
+++ myapp5/src/App.jsx
@@ -2,31 +2,45 @@ import "./App.css"
 import { useState } from 'react';
 
 function App() {
-   const [first, second] = useState("Hello")
-   const [firstStyle, hoverStyle] = useState(false)
-  
-  function changeText (){
-    second("bye")
-  }
-  function hover (){
+ 
+  const [first , second]  = useState("Hello")
+  const [firstStyle, hoverStyle] = useState(false)
+  const [name, useName] = useState("")
+
+   function hover (){
     hoverStyle(true)
   }
   function out (){
     hoverStyle(false)
   }
 
+  function changeText(){
+    second("bye")
+  }
 
 
+  function changedText(event){
+  useName(event.target.value)
+  }
+
+
+ 
   return (
 <div> 
-    <h1>{first}</h1>
-  <input type="text" placeholder='Enter your name' />
-  <button onClick={changeText} onMouseOut={out} onMouseOver={hover} style={{backgroundColor : firstStyle ? "black" : "white" }}>Submit</button>
+    <h1>{first} {name}</h1>
+  <input type="text" placeholder='Enter your name' onChange={changedText}  />
+  <button onClick={changedText} onMouseOut={out} onMouseOver={hover} style={{backgroundColor : firstStyle ? "black" : "white" }}>Submit</button>
 </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
 
 
 
