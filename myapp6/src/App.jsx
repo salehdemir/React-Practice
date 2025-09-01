386e28ec664e535ade9db65b923a6da4851b4263
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-09-01 11:00:42 +0430

    default style page

--- myapp6/src/App.jsx
+++ myapp6/src/App.jsx
@@ -1,13 +1,46 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [isActive , setIsactive] = useState(true)
+
   
 
   return (
     <>
-    <h1>hello</h1>
+<button className={isActive ? "active" : "inActive"} onClick={ ()=> setIsactive(!isActive)}>Click Me</button>
+ 
     </>
   )
 }
 
 export default App
+
+
+
+
+
+  //  1: Toggle btn
+  //  const [isVisible , setIsvisible] = useState(false)
+  //   return (
+  //   <>
+  // <button onClick={()=> setIsvisible(!isVisible)}>Toggle</button>
+
+  // {isVisible && <div>Content to show or hide</div>}
+  //   </>
+  // )
+
+
+
+// 2: input value
+  //   const [value , setValue] = useState("")
+  // function changHandle (e){
+  //  setValue(e.target.value)
+  // }
+  //   return (
+  //   <>
+  // <input type="text"  value={value} onChange={changHandle} />
+ 
+  //   </>
+  // )
