fc7e93afc9e71300c25b9892b4dc6f4f2e80adaf
Saleh <103447502+salehdemir@users.noreply.github.com>
2025-09-07 11:55:22 +0430

    practice routing

--- myapp6/src/App.jsx
+++ myapp6/src/App.jsx
@@ -3,51 +3,66 @@
 //2. When the add button is pressed, the current data in the input should be
 //added to an array.
 //3. The <ul> should display all the array items as <li>s
-import { useState } from "react";
+
 import "./App.css"
+
 function App() {
-
-const [inputText , setInputText] = useState("")
-const [items , setitems] = useState([])
-
-function changeHandle(event){
- const inputValue = event.target.value
- setInputText(inputValue)
-}
-
-function addItems (){
-  setitems (prev => {
-return[
-  ...prev,
-inputText
-  ]
-  },
-  setInputText("")
+return(
+  <>
+ <div>
+  <h1>Main page</h1>
+ </div>
+  </>
 )
 }
 
-
-
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text"  onChange={changeHandle} value={inputText}/>
-        <button onClick={addItems}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-         {items.map(item =>{
-         return <li>{item}</li>
-         })}
-        </ul>
-      </div>
-    </div>
-  );
-}
-
 export default App;
+
+
+
+
+
+// 1: Prev Seperate operators
+// import { useState } from "react";
+// const [inputText , setInputText] = useState("")
+// const [items , setitems] = useState([])
+
+// function changeHandle(event){
+//  const inputValue = event.target.value
+//  setInputText(inputValue)
+// }
+
+// function addItems (){
+//   setitems (prev => {
+// return[
+//   ...prev,
+//   inputText
+//   ]
+//   },
+//   setInputText("")
+// )
+// }
+
+
+
+//   return (
+//     <div className="container">
+//       <div className="heading">
+//         <h1>To-Do List</h1>
+//       </div>
+//       <div className="form">
+//         <input type="text"  onChange={changeHandle} value={inputText}/>
+//         <button onClick={addItems}>
+//           <span>Add</span>
+//         </button>
+//       </div>
+//       <div>
+//         <ul>
+//          {items.map(item =>{
+//          return <li>{item}</li>
+//          })}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+
--- myapp6/src/main.jsx
+++ myapp6/src/main.jsx
@@ -1,9 +1,38 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import About from './about.jsx'
+import Error from './error.jsx'
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+
+const route = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+       
+    },
+    {
+        path: "/about",
+        element: <About/>
+    },
+    {
+       path: "/home",
+       element: <App/>
+    },
+    {
+        path: "*",
+        element: <Error/>
+    }
+    
+
+
+   
+])
+
+
 
 createRoot(document.getElementById('root')).render(
 
-    <App />
+   <RouterProvider router={route}/>
  
 )
